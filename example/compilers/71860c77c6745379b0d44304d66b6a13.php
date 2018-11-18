cde1ccf47dbc435a504e9d4fb9a4cf80ad0419bb
Davide Cesarano <davide.cesarano@unipegaso.it>
2018-11-18 17:52:26 +0100

    Remove compilers

--- example/compilers/71860c77c6745379b0d44304d66b6a13.php
+++
@@ -1,9 +0,0 @@
-<?php $this->include('header', ['b' => 'cccccc']); ?>
-
-<p>Ciao <?php echo htmlentities($message); ?></p>
-
-<?php if($a == 1): ?>
-    <p>Ciao come stai?</p>
-<?php else: ?> 
-    <p>Noooo</p>
-<?php endif; ?>
